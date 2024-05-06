29ef2fb8f83dea509d90cc7745d643f9b5d4a7f1
dimas iqbal jailani <163813549+Greenteacore@users.noreply.github.com>
2024-05-06 17:58:14 +0700

    Update and rename HitungVokal.java to Modul5HitungVokal.java

--- Modul5HitungVokal.java
+++ Modul5HitungVokal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HitungVokal {
+public class Modul5HitungVokal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
