9210b6ae5ffa0211893f0f8185a2fdeab573b6fe
dimas iqbal jailani <163813549+Greenteacore@users.noreply.github.com>
2024-05-06 17:54:59 +0700

    Update and rename CekBilanganPrima.java to Modul5CekBilanganPrima.java

--- Modul5CekBilanganPrima.java
+++ Modul5CekBilanganPrima.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CekBilanganPrima {
+public class Modul5CekBilanganPrima {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
